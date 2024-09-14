266c21be1696cf5681d07c6b3971180c13f7ceb0
ACE <acedapogi@yahoo.com>
2024-09-14 10:36:49 +0800

    Initial commit.

---
+++ src/main/java/com/ace/test/sqldemo/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.ace.test.sqldemo.controller;
+
+import com.ace.test.sqldemo.data.User;
+import com.ace.test.sqldemo.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import  java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public List<User> getAllUsers() {
+
+        return userRepository.findAll();
+    }
+
+}
---
+++ src/main/java/com/ace/test/sqldemo/data/UserRepository.java
@@ -0,0 +1,6 @@
+package com.ace.test.sqldemo.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
